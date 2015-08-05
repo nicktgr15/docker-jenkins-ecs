1422421e620a3881e002df2cff3d5e02778b1eda
nicktgr15 <nicktgr15@gmail.com>
2015-08-05 18:55:48 +0100

    Using sys.stdout.write

--- deploy.py
+++ deploy.py
@@ -3,6 +3,7 @@
 from time import sleep
 import boto3
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--service-name", dest='service_name', required=True)
@@ -10,18 +11,23 @@ args = parser.parse_args()
 
 client = boto3.client('ecs', region_name='eu-west-1')
 
-print("Setting desiredCount=0")
+sys.stdout.write("Setting desiredCount=0\n")
+sys.stdout.flush()
 client.update_service(service=args.service_name, desiredCount=0)
 
 while client.update_service(service=args.service_name)['service']['runningCount'] != 0:
-    print("runningCount!=0, sleeping")
+    sys.stdout.write("runningCount!=0, sleeping\n")
+    sys.stdout.flush()
     sleep(5)
 
-print("Setting desiredCount=1")
+sys.stdout.write("Setting desiredCount=1\n")
+sys.stdout.flush()
 client.update_service(service=args.service_name, desiredCount=1)
 
 while client.update_service(service=args.service_name)['service']['runningCount'] == 0:
-    print("runningCount=0, sleeping")
+    sys.stdout.write("runningCount=0, sleeping\n")
+    sys.stdout.flush()
     sleep(5)
 
-print("runningCount>0, Done!!")
+sys.stdout.write("runningCount>0, Done!!\n")
+sys.stdout.flush()
